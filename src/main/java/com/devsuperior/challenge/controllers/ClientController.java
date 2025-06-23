988d4187681ba497846830b45e23c3303082821a
Cristiano Araujo <crisd.silva75@gmail.com>
2025-06-23 14:21:48 -0300

    INSERT and PUT

--- src/main/java/com/devsuperior/challenge/controllers/ClientController.java
+++ src/main/java/com/devsuperior/challenge/controllers/ClientController.java
@@ -1,16 +1,24 @@
 package com.devsuperior.challenge.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.challenge.dto.ClientDTO;
 import com.devsuperior.challenge.services.ClientService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -28,6 +36,20 @@ public class ClientController {
 	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
 		Page<ClientDTO> dto = service.findAll(pageable);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO dto) {
+		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
 }
